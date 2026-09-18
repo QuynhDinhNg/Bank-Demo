98377355fcb29be809ec8fd7f2a1e87075901ff0
Dinh Quynh <dnnquynh02@gmail.com>
2026-09-18 10:15:12 +0700

    Initial commit

---
+++ src/test/java/baseTest/baseTest.java
@@ -0,0 +1,35 @@
+package baseTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class baseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Properties props;
+    ;
+
+    @Before
+    public void setUp() throws IOException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        props = new Properties();
+        props.load(new FileInputStream("src/test/resources/config.properties"));
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+
+    }
+}
